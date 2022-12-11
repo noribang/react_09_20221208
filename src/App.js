86d4c5398075c112c5ef9d772f572989258b1588
hoonkim <hoonk9387@gmail.com>
2022-12-11 13:35:21 -0600

    Loop through pets array to rener list of petcard components.

--- src/App.js
+++ src/App.js
@@ -15,27 +15,22 @@ const pets = [
   { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg" }
 ]
 
+// Loop thorugh pets array.
+const petCards = pets.map((petsObj) => { 
+  return <PetCard key={petsObj.id} name={petsObj.name} image={petsObj.image}/> 
+});
+console.log("petCards:", petCards)
 
-export default function App() {
+export default function App() { 
   return (
     <div>
       <Header />
       <main>
-        <PetCard name={pets[0].name} image={pets[0].image}/>
+        {petCards}
+        {/* <PetCard name={pets[0].name} image={pets[0].image}/>
         <PetCard name={pets[1].name} image={pets[1].image}/>
-        <PetCard name={pets[2].name} image={pets[2].image}/>
+        <PetCard name={pets[2].name} image={pets[2].image}/> */}
       </main>
-      {/* <main>
-        <Card title="Welcome to PetLand!">
-          <em>Find your dream pet</em>
-        </Card>
-        <Card title="What pets would you like to see?">
-          <div>
-            <EmojiButton emoji="😸" label="Cats" />
-            <EmojiButton emoji="🐕" label="Dogs" />
-          </div>
-        </Card>
-      </main> */}
     </div>    
   );
 }
