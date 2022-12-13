7c8c040299a5ccb4679eb2f6978eeae93a956586
hoonkim <hoonk9387@gmail.com>
2022-12-12 21:10:12 -0600

    Hard coding passing in props from array of objects.

--- src/App.js
+++ src/App.js
@@ -9,36 +9,27 @@ import PetCard from './PetCard';
 import './style.css';
 
 // Array to be used in PetCards.
-// const pets = [
-//   { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg" },
-//   { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg"},
-//   { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg" }
-// ]
-
 const pets = [
-  { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg", favSnacks: ["Peanut Butter", "Soccer Balls"] },
-  { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg", favSnacks: ["Cake", "Bacon"]},
-  { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg", favSnacks: ["Pigeons", "Grass", "Chicken"] }
+  { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg" },
+  { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg"},
+  { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg" }
 ]
 
+// const pets = [
+//   { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg", favSnacks: ["Peanut Butter", "Soccer Balls"] },
+//   { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg", favSnacks: ["Cake", "Bacon"]},
+//   { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg", favSnacks: ["Pigeons", "Grass", "Chicken"] }
+// ]
 
-// Loop thorugh pets array.
-const petCards = pets.map((petsObj) => { 
-  return <PetCard key={petsObj.id} name={petsObj.name} image={petsObj.image}/> 
-});
-console.log("petCards:", petCards)
-
-export default function App() { 
+function App() {
   return (
     <div>
       <Header />
       <main>
-        {petCards}
-        {/* <PetCard name={pets[0].name} image={pets[0].image}/>
-        <PetCard name={pets[1].name} image={pets[1].image}/>
-        <PetCard name={pets[2].name} image={pets[2].image}/> */}
+        <PetCard name={pets[0].name} image={pets[0].image} />
+        <PetCard name={pets[1].name} image={pets[1].image} />
       </main>
-    </div>    
+    </div>
   );
 }
 
@@ -50,4 +41,4 @@ const sayHi = "Speaking in Hello";
 
 // Named export.
 export { logger, sayHi}
-// export default App;
+export default App;
--- src/PetCard.js
+++ src/PetCard.js
@@ -5,9 +5,11 @@ export default function PetCard(props) {
     return (
         <div className="card">
             <h2>{props.name}</h2>
-            <img src={props.image} alt={props.name}/>
+            <img src={props.image}/>
         </div>
     )
 }
+
+
 
 // export default PetCard;
